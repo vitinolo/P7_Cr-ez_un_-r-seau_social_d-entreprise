a44803d9d398528458521938f51fc1c67c56deec
thibaut lenoir <lenoir677@gmail.com>
2022-08-21 18:26:55 +0200

    icones svg fontawesome

--- frontend/src/components/Comment.js
+++ frontend/src/components/Comment.js
@@ -2,10 +2,10 @@ import "../styles/style.css";
 function Comment(){
     return(
     <div className="comment">
-            <div className="profile_img">photo</div>
-            <span className="profile_name">angelo</span>
+            <div className="profile_img">photo d'</div>
+            <span className="profile_name">angelo </span>
             <span className="date_edition">08/09/2002</span>
-            <div className="img_publication"></div>
+            <div className="img_publication">Ceci est une image</div>
             <div className="texte_publication">ceci est un commentaire !</div>
     </div>);
 }
--- frontend/src/components/Feed.js
+++ frontend/src/components/Feed.js
@@ -1,32 +1,35 @@
 import "../styles/style.css";
 import Comments from "../components/Comments";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faComment, faTrash, faPen, faThumbsUp,faThumbsDown } from '@fortawesome/free-solid-svg-icons'
+
 
 function Feed() {
   return (
     <div className="feed_publication">
         <div className="profile">
-            <div className="profile_img"></div>
+            <div className="profile_img">photo de</div>
             <span className="profile_name">marcello</span>
             <span className="date_edition">08/09/2002</span>
         </div>
         <div className="img_publication">
-
+          Ceci est une image
         </div>
         <div className="texte_publication">
-          ça va bien ?
+          Ceci est un message
         </div>
-          <div className="modifyAndRemovePost">
-            <div className="icone"><FontAwesomeIcon icon="fa-solid fa-trash-can" /></div>
-            <div className="icone"><FontAwesomeIcon icon="fa-solid fa-pen-to-square" /></div>
-          </div>
+        <div className="modifyAndRemovePost">
+
+          <span className="icone-comment icone"><FontAwesomeIcon icon={faComment} /></span>
+          <span className="icone-remove icone"><FontAwesomeIcon icon={faTrash} /></span>
+          <span className="icone-modify icone"><FontAwesomeIcon icon={faPen} /></span>
+        </div>
         <div className="vote_publication">
-          <div className="icone"><FontAwesomeIcon icon="fa-solid fa-thumbs-up" /></div>
-          <div className="icone"><FontAwesomeIcon icon="fa-solid fa-thumbs-down" /></div>
+          <span className="icone-like icone"><FontAwesomeIcon icon={faThumbsUp} /></span>
+          <span className="icone-dislike icone"><FontAwesomeIcon icon={faThumbsDown} /></span>
         </div>
         <Comments />
     </div>
-      
 
   );
 }
